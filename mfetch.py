c4778a0faf75b3e2956b4b57213af2224eac4188
Mia Lane <fillane77@gmail.com>
2020-06-19 13:20:54 +0200

    big commit
    - Removed Arch detection - adds too much bloat
    - Removed unneeded logic and bloat
    + Improved logic

--- mfetch.py
+++ mfetch.py
@@ -2,98 +2,73 @@ import platform
 import getpass
 from uptime import uptime
 
-
 spf = platform.platform()
-sarch = platform.machine()
-proc = platform.processor()
+if "indows-7" in spf:
+    spf = "Windows 7"
+elif "indows-10" in spf:
+    spf = "Windows 10"
+
+sys = platform.machine()
+prc = platform.processor()
 usr = getpass.getuser()
 hst = platform.node()
-ut = uptime()
-utm = int(ut/60%60)
-uth = int(ut/60/60%24)
-utd = int(ut/60/60/24)
+upt = uptime()
+utm = int(upt/60%60)
+uth = int(upt/60/60%24)
+utd = int(upt/60/60/24)
 inf = ""
 
+if "indows" in spf:
 
-if "Windows" in spf:
+    inf = """           
+           _.-;;-._     {}@{}
+    '-..-'|   ||   |    |-sys: {}
+    '-..-'|_.-;;-._|    |-arc: {}
+    '-..-'|   ||   |    |-prc: {}
+    '-..-'|_.-''-._|    |-upt: {} days, {} hours, {} minutes
+    """
 
-    if "Windows-7" in spf:
-        spf = 'Windows 7'
-        inf = """                      
-                 _.-;;-._      {}@{}
-                |   ||   |     platform: {}
-                |_.-77-._|     architecture: {}
-                |   ||   |     processor: {}
-                |_.-''-._|     uptime: {} days, {} hours, {} minutes
-        """
-
-    elif "Windows-10" in spf:
-        spf = 'Windows 10'
+    if "Windows 10" in spf:
         inf = """           
-                      _.-:     {}@{}
-                 _.-:|   |     platform: {}
-                |   ||_.-|     architecture: {}
-                |_.-||   |     processor: {}
-                |   ||_.-'     uptime: {} days, {} hours, {} minutes
-                |_.-'
+                  _.-:    {}@{}
+             _.-:|   |    |-sys: {}
+            |   ||_.-|    |-arc: {}
+            |_.-||   |    |-prc: {}
+            |   ||_.-'    |-upt: {} days, {} hours, {} minutes
+            |_.-'
         """
-
-    else:
-        inf = """
-               _.-;;-._      {}@{}
-        '-..-'|   ||   |     platform: {}
-        '-..-'|_.-;;-._|     architecture: {}
-        '-..-'|   ||   |     processor: {}
-        '-..-'|_.-''-._|     uptime: {} days, {} hours, {} minutes
-        """
-    
-    
-        
 
 elif "inux" in spf:
 
-    if "Arc" in spf:
-
-        inf = """
-       -  -  - -  - - /\\         {}@{}
-		     /  \\        platform: {}
-	            /\\   \\       architecture: {}
-		   /      \\      processor: {}
-		  /   ,,   \\     uptime: {} days, {} hours, {} minutes
-   	    -  - /   |  |  -\\
-		/_-''    ''-_\\
-        """
-       
-
-    else:
-
-        inf = """
-             .---.          {}@{}
-            /     \         platform: {}
-            \.@-@./         architecture: {}
-            /`\_/`\         processor: {}
-           //  _  \\\        uptime: {} days, {} hours, {} minutes
+    inf = """
+             .---.         {}@{}
+            /     \        |-sys: {}
+            \.@-@./        |-arc: {}
+            /`\_/`\        |-prc: {}
+           //  _  \\\      |-upt: {} days, {} hours, {} minutes
           | \     )|_ 
          /`\_`>  <_/ \\
          \__/'---'\__/
-        """
+    """
         
 
 elif "Mac" in spf:
 
     inf = """
    			        .:'             
-			     _ :'_         {}@{}
-			.'\`_\`-'_\`\      platform: {}
-			:________.-'       architecture: {}
-			:_______:          processor: {}
-			 :_______\`-;      uptime: {} days, {} hours, {} minutes
-			  \`._.-._.'
-        """
+			     _ :'_      
+			.'\`_\`-'_\`\    {}@{}
+			:        .-'     |-sys: {}
+			:        :       |-arc: {}
+			 :        `-;    |-prc: {}
+			  `._.-._.'      |-upt: {} days, {} hours, {} minutes
+    """
+
 else:
     print("unsupported system!")  
 
-print(inf.format(usr, hst, spf, sarch, proc, utd, uth, utm)) 
+print(inf.format(usr, hst, spf, sys, prc, utd, uth, utm)) 
+
 
 
 
