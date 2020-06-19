529d3141d4fc8b5720409063d841321c3e994768
Mia Lane <fillane77@gmail.com>
2020-06-19 09:21:55 +0200

    Fixes/Additions
    + Added MacOS and Arch linux support
    + Added an error message for unsupported systems (Might change)

--- mfetch.py
+++ mfetch.py
@@ -2,6 +2,7 @@ import platform
 import getpass
 from uptime import uptime
 
+
 spf = platform.platform()
 sarch = platform.machine()
 proc = platform.processor()
@@ -11,48 +12,90 @@ ut = uptime()
 utm = int(ut/60%60)
 uth = int(ut/60/60%24)
 utd = int(ut/60/60/24)
+inf = ""
 
 
-if "Windows-7" in spf:
-    print("""                       
-             _.-;;-._      {}@{}
-            |   ||   |     platform: {}
-            |_.-77-._|     architecture: {}
-            |   ||   |     processor: {}
-            |_.-''-._|     uptime: {} days, {} hours, {} minutes
-    """.format(usr, hst, spf, sarch, proc, utd, uth, utm))
+if "Windows" in spf:
 
-elif "Windows-10" in spf:
-    print("""           
-                  _.-;     {}@{}
-             _.-;|   |     platform: {}
-            |   ||_.-|     architecture: {}
-            |_.-||   |     processor: {}
-            |   ||_.-'     uptime: {} days, {} hours, {} minutes
-            |_.-'
-    """.format(usr, hst, spf, sarch, proc, utd, uth, utm))
+    if "Windows-7" in spf:
+        spf = 'Windows 7'
+        inf = """                      
+                 _.-;;-._      {}@{}
+                |   ||   |     platform: {}
+                |_.-77-._|     architecture: {}
+                |   ||   |     processor: {}
+                |_.-''-._|     uptime: {} days, {} hours, {} minutes
+        """
 
-elif "Windows" in spf:
-        print("""
-             _.-;;-._      {}@{}
-      '-..-'|   ||   |     platform: {}
-      '-..-'|_.-;;-._|     architecture: {}
-      '-..-'|   ||   |     processor: {}
-      '-..-'|_.-''-._|     uptime: {} days, {} hours, {} minutes
-    """.format(usr, hst, spf, sarch, proc, utd, uth, utm))
+    elif "Windows-10" in spf:
+        spf = 'Windows 10'
+        inf = """           
+                      _.-:     {}@{}
+                 _.-:|   |     platform: {}
+                |   ||_.-|     architecture: {}
+                |_.-||   |     processor: {}
+                |   ||_.-'     uptime: {} days, {} hours, {} minutes
+                |_.-'
+        """
+
+    else:
+        inf = """
+               _.-;;-._      {}@{}
+        '-..-'|   ||   |     platform: {}
+        '-..-'|_.-;;-._|     architecture: {}
+        '-..-'|   ||   |     processor: {}
+        '-..-'|_.-''-._|     uptime: {} days, {} hours, {} minutes
+        """
     
+    
+        
 
-elif "Linux" in spf:
-    print("""
-            .---.          {}@{}
-           /     \         platform: {}
-           \.@-@./         architecture: {}
-           /`\_/`\         processor: {}
-          //  _  \\        uptime: {} days, {} hours, {} minutes
-         | \     )|_ 
-        /`\_`>  <_/ \
-        \__/'---'\__/
-    """.format(usr, hst, spf, sarch, proc, utd, uth, utm))
+elif "inux" in spf:
+
+    if "Arc" in spf:
+
+        inf = """
+       -  -  - -  - - /\\         {}@{}
+		     /  \\        platform: {}
+	            /\\   \\       architecture: {}
+		   /      \\      processor: {}
+		  /   ,,   \\     uptime: {} days, {} hours, {} minutes
+   	    -  - /   |  |  -\\
+		/_-''    ''-_\\
+        """
+       
+
+    else:
+
+        inf = """
+             .---.          {}@{}
+            /     \         platform: {}
+            \.@-@./         architecture: {}
+            /`\_/`\         processor: {}
+           //  _  \\\        uptime: {} days, {} hours, {} minutes
+          | \     )|_ 
+         /`\_`>  <_/ \\
+         \__/'---'\__/
+        """
+        
+
+elif "Mac" in spf:
+
+    inf = """
+   			        .:'             
+			     _ :'_         {}@{}
+			.'\`_\`-'_\`\      platform: {}
+			:________.-'       architecture: {}
+			:_______:          processor: {}
+			 :_______\`-;      uptime: {} days, {} hours, {} minutes
+			  \`._.-._.'
+        """
+else:
+    print("unsupported system!")  
+
+print(inf.format(usr, hst, spf, sarch, proc, utd, uth, utm)) 
+
+
 
 
 
